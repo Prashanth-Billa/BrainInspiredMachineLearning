b087c3343d2944694ce56909176fce01ae19abf9
Prashanth-Billa <pbilla@uci.edu>
2016-11-30 01:38:30 -0800

    cleanup

--- Project/Constants.py
+++ Project/Constants.py
@@ -13,7 +13,7 @@ taupost = 14 * ms
 apre = 103e-2
 apost = -40
 
-NumOfDigitsTrain = 750
+NumOfDigitsTrain = 800
 NumOfDigitsTest = 10
 
 NUM_OUTPUT_CLASSES = len(DIGITS)
@@ -24,31 +24,34 @@ for i in range(0, NUM_OUTPUT_CLASSES-1):
     
 timeInh2 = []
 for i in range(0, NUM_OUTPUT_CLASSES-1):
-    timeInh2.append(40)
+    timeInh2.append(30)
     
 timeInh3 = []
 for i in range(0, NUM_OUTPUT_CLASSES-1):
-    timeInh3.append(70)
+    timeInh3.append(60)
     
 timeInh4 = []
 for i in range(0, NUM_OUTPUT_CLASSES-1):
-    timeInh4.append(100)
+    timeInh4.append(90)
     
 timeInh5 = []
 for i in range(0, NUM_OUTPUT_CLASSES-1):
-    timeInh5.append(130)
+    timeInh5.append(120)
     
 timeInh6 = []
 for i in range(0, NUM_OUTPUT_CLASSES-1):
-    timeInh6.append(160)
-    
+    timeInh6.append(145)
 
+timeInh7 = []
+for i in range(0, NUM_OUTPUT_CLASSES-1):
+    timeInh7.append(170)
 
 timeExh1 = [3]
-timeExh2 = [40]
-timeExh3 = [70]
-timeExh4 = [100]
-timeExh5 = [130]
-timeExh6 = [160]
+timeExh2 = [30]
+timeExh3 = [60]
+timeExh4 = [90]
+timeExh5 = [120]
+timeExh6 = [145]
+timeExh7 = [170]
 
-Syn12Condition = 'I += 50 * volt/second'
+Syn12Condition = 'I += 25 * volt/second'
